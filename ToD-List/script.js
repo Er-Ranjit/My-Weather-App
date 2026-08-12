e54c88595819d0201912174e637ed61d33a1b685
Er-Ranjit <ranjitkumar728064@gmail.com>
2026-08-12 13:35:50 +0530

    My ToDo App Update

--- ToD-List/script.js
+++ ToD-List/script.js
@@ -4,6 +4,7 @@ const list = document.getElementById("todoList");
 
 let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
+
 todos.forEach((todo) => {
     showTodo(todo);
 });
@@ -20,34 +21,54 @@ button.addEventListener("click", () => {
     };
 
     todos.push(newTask);
-    localStorage.setItem("todos", JSON.stringify(todos));
+    saveLocalStorageData();
 
     input.value = "";
     showTodo(newTask);
 });
 
 
+function saveLocalStorageData() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function ToDoComplete(todo, textElement) {
+    todo.completed = !todo.completed;
+
+    if (todo.completed) {
+        textElement.style.textDecoration = "line-through";
+        textElement.style.color = "black";
+    } 
+    
+    saveLocalStorageData();
+}
+function ToDoRemove(todo, element) {
+    if (!todo.completed) {
+        alert("Please click on this task and complete it first only then will it be deleted!");
+        return; 
+    }else{
+        alert("Task Delete Success Full!");
+
+    }
+    todos = todos.filter(e => e.id !== todo.id);
+    saveLocalStorageData();
+    element.remove();
+}
+
 function showTodo(todo) {
     const li = document.createElement("li");
 
     li.innerHTML = `
-        ${todo.text}
-        <button  type="button" onclick="deleteTodo(${todo.id})">
-            Delete
-        </button>
+        <span class="task-text" style="${todo.completed ? 'text-decoration: line-through; color: gray;' : ''}">
+            ${todo.text}
+        </span>
+        <button class="delete-btn">Delete</button>
     `;
 
+    const taskText = li.querySelector(".task-text");
+    const deleteBtn = li.querySelector(".delete-btn");
+    taskText.addEventListener("click", () => ToDoComplete(todo, taskText));
+    deleteBtn.addEventListener("click", () => ToDoRemove(todo, li));
+
     list.appendChild(li);
-}
-
-function deleteTodo(id) {
-    todos = todos.filter(todo => todo.id !== id);
-
-    localStorage.setItem("todos", JSON.stringify(todos));
-
-    list.innerHTML = "";
-
-    todos.forEach((todo) => {
-        showTodo(todo);
-    });
 }
